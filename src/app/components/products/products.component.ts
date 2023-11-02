58a36de473640434b23fa4afd75e6cc88a62e2d0
SSProgramador <sebastian.stremel@gmail.com>
2023-11-01 22:25:34 -0300

    se crea carrito de compras

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -31,6 +31,8 @@ export class ProductsComponent implements OnInit{
 
   public Products : any= [];
 
+  public carrito : any = [];
+
   constructor(private productsService: ProductsService){
 
     this.productsService.getProducts().subscribe(data => {
@@ -44,4 +46,61 @@ export class ProductsComponent implements OnInit{
 
     console.log("productos", this.Products)
   } 
+
+
+  addProduct(id:any, nombre:string, descripcion:string, precio:number): void {
+    //buscar en el arreglo del localstorage si el producto ya fue agregado
+    const localStorageCarrito = localStorage.getItem("carrito")
+    if (localStorageCarrito === null){ //el carrito estaba vacio
+      const prodToCart = {idProducto:id, nombre, descripcion, precio, cantProd:1}
+      this.carrito.push(prodToCart)
+      localStorage.setItem("carrito", JSON.stringify(this.carrito))
+    }else{ 
+      console.log("entro por aca")
+      const prods:any= JSON.parse(localStorageCarrito) 
+      console.log("productos", prods)
+  
+      const index = prods.findIndex((prod:any) => prod.idProducto === id)
+      const productoEncontrado : any[] = prods.filter((prod:any) => prod.idProducto === id)
+      if (productoEncontrado.length > 0){
+        productoEncontrado[0].cantProd = productoEncontrado[0].cantProd + 1
+        //gaurdar el producto
+        prods.splice(index,1)
+        prods.push(productoEncontrado[0])
+        
+        localStorage.removeItem("carrito")
+        localStorage.setItem("carrito", JSON.stringify(prods))
+      }else{
+        const prodToCart = {idProducto:id, nombre, descripcion, precio, cantProd:1}
+        prods.push(prodToCart)
+        localStorage.removeItem("carrito")
+        localStorage.setItem("carrito", JSON.stringify(prods))        
+      }
+    }
+
+  }
+
+  removeProduct(id:any):void {
+    const localStorageCarrito = localStorage.getItem("carrito")
+    if (localStorageCarrito != null){ //el carrito estaba vacio
+      const prods:any= JSON.parse(localStorageCarrito) 
+      const index = prods.findIndex((prod:any) => prod.idProducto === id)
+      const productoEncontrado : any[] = prods.filter((prod:any) => prod.idProducto === id)
+      if (productoEncontrado.length > 0){
+
+        if (productoEncontrado[0].cantProd === 1){
+          //eliminar el producto del carrito
+          prods.splice(index,1)
+        }else{ 
+          productoEncontrado[0].cantProd = productoEncontrado[0].cantProd - 1
+          prods.splice(index,1)
+          prods.push(productoEncontrado[0])
+        }
+      
+        //gaurdar el producto
+        localStorage.removeItem("carrito")
+        localStorage.setItem("carrito", JSON.stringify(prods))      
+      }
+    }
+  }  
 }
---
+++ src/readme.md
@@ -0,0 +1,14 @@
+#Para el carrito - Opcion 1
+
+crear un componente para listar lo que tiene el carrito
+
+1) buscas en localstorage key "carrito" => [{},{},{}]
+
+2) transformas a objecto lo que devolvio el localstorage JSON.parse()
+
+3) recorres el arreglo y por cada elemento del arreglo con el id, vas a buscar a la bd el nombre, descripcion
+
+
+#Para el carrito - Opcion 2
+
+1) Leer directo del localstorage
