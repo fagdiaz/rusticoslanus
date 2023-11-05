c290931f1b91d86010a95df700de1b7ac5c1d042
SSProgramador <sebastian.stremel@gmail.com>
2023-11-05 12:24:49 -0300

    add login with firebase

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -5,18 +5,43 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { User } from 'src/app/entity/user';
 import { TokenGuard } from 'src/app/guard/token.guard';
 import { ProductsService } from 'src/app/services/products.service';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9uk6RPuERTLj9HCXUToCOmhIfjkqS3ok",
+
+  authDomain: "rusticoslanus-84470.firebaseapp.com",
+
+  projectId: "rusticoslanus-84470",
+
+  storageBucket: "rusticoslanus-84470.appspot.com",
+
+  messagingSenderId: "261854181198",
+
+  appId: "1:261854181198:web:4a0405bf75420df955901f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+//const db = getFirestore(app);
+
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent {
 
+export class SigninComponent {
+  
+
+  
   public user: User = new User();
   public signinVar = false;
 
-  public signin() {
+  /*public signin() {
     this.http.post<any>('http://localhost:3000/signin', this.user).subscribe(
       (response) => {
         const token = response.token;
@@ -32,7 +57,7 @@ export class SigninComponent {
         console.error('Error al realizar la solicitud:', error);
       }
     );
-  }
+  }*/
 /*
   delete() {
     this.deleteVar.deleteUser();
@@ -45,5 +70,14 @@ export class SigninComponent {
     public http: HttpClient,
     public jwt: JwtHelperService
   ) {}
+
+
+  public signin = async () => {
+    const auth = getAuth();
+    const resSignIn = await signInWithEmailAndPassword(auth, "seba", "123");
+    console.log("signIN", resSignIn);
+    return resSignIn
+  }
+
 
 }
